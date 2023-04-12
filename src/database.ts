e8d3af686da72496010c002b68a0e585624d6b8f
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-12 15:40:51 +0200

    Connected to MySQL

---
+++ src/database.ts
@@ -0,0 +1,25 @@
+// @ts-nocheck
+import dotenv from "dotenv";
+import { createConnection } from "mysql";
+
+dotenv.config();
+
+let DB_NAME;
+if (process.env.NODE_ENV.trim() === "testing") {
+  DB_NAME = process.env.DB_NAME_TSESTING.trim();
+} else {
+  DB_NAME = process.env.DB_NAME.trim();
+}
+
+const HOST = process.env.DB_HOST;
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+let conn = createConnection({
+  host: HOST,
+  user: USERNAME,
+  password: PASSWORD,
+  database: DB_NAME,
+});
+
+export default conn;
