5db5b2e257a0cfee930be109c625e2a5055f6938
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-14 05:23:10 +0200

    Email: Unique

--- migrations/sqls/20230413041402-users-up.sql
+++ migrations/sqls/20230413041402-users-up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY NOT NULL,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     gender VARCHAR(255) DEFAULT 'Male',
