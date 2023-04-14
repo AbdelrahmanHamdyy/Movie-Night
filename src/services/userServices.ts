780426f38c7ee936e69609e3ef3ef23fb999a06f
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-14 05:10:19 +0200

    Verify user service function

---
+++ src/services/userServices.ts
@@ -0,0 +1,39 @@
+import { generateJWT, generateVerifyToken } from "../utils/generateTokens";
+import { sendVerifyEmail } from "../utils/emails";
+import { UserData } from "../models/User";
+
+type responseObject = {
+  statusCode: number;
+  body: Object;
+};
+
+export async function verifyUser(user: UserData): Promise<responseObject> {
+  if (!user) {
+    return {
+      statusCode: 400,
+      body: {
+        error: "User not found",
+      },
+    };
+  }
+  const token = await generateVerifyToken(user.id, "verifyEmail");
+  const emailSent = sendVerifyEmail(user, token);
+
+  if (!emailSent) {
+    return {
+      statusCode: 400,
+      body: {
+        error: "Failed to send verification email!",
+      },
+    };
+  }
+
+  const jwtToken = generateJWT(user);
+  return {
+    statusCode: 201,
+    body: {
+      username: user.username,
+      token: jwtToken,
+    },
+  };
+}
