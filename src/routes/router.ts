c8e5f35f0809e53a609bda6af2892d31fcd9f76e
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-15 03:08:21 +0200

    Add loginRouter to mainRouter

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { Request, Response } from "express";
 import signupRouter from "./signup";
+import loginRouter from "./login";
 
 const mainRouter = express.Router();
 
 mainRouter.use(signupRouter);
+mainRouter.use(loginRouter);
 
 mainRouter.use((req: Request, res: Response) => {
   res.status(404).json(`Can't ${req.method} ${req.url}`);
