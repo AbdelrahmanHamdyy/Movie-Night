10d7ff862fef55e49149673b41ff9bfbf31c5154
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-15 22:28:42 +0200

    Admin with permissions

---
+++ migrations/sqls/20230415195420-admin-permissions-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE admin_permissions (
+    admin_id INT NOT NULL REFERENCES users(id),
+    permission VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+    CONSTRAINT PK PRIMARY KEY (admin_id, permission)
+);
