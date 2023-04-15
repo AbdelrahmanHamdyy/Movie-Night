0da1819abb67a7718a17c213f2dfc402f3f64d16
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-15 05:22:25 +0200

    Reset password endpoint working

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { User, UserData } from "../models/User";
 import {
   authenticateUser,
+  changePassword,
+  checkUserById,
+  checkVerificationToken,
   forgetPasswordEmail,
   verifyUsernameAndEmail,
 } from "../services/userServices";
@@ -34,8 +37,35 @@ const forgetPassword = async (req: Request, res: Response) => {
     const email = req.body.email as string;
     const user = await verifyUsernameAndEmail(username, email);
     await forgetPasswordEmail(user);
-    const token = generateJWT(user);
     return res.status(200).json("Reset Password Email sent successfully");
+  } catch (error: any) {
+    if (error.statusCode === 400) {
+      res.status(error.statusCode).json({ error: error.message });
+    } else if (error.statusCode) {
+      res.status(error.statusCode).json(error.message);
+    } else {
+      res.status(500).json("Internal server error");
+    }
+  }
+};
+
+const resetPassword = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id as unknown as number;
+    const token = req.params.token as string;
+
+    const newPassword = req.body.newPassword;
+    const verifyPassword = req.body.verifyPassword;
+
+    const user = await checkUserById(id);
+    await checkVerificationToken(
+      user,
+      token,
+      "resetPassword",
+      newPassword,
+      verifyPassword
+    );
+    return res.status(200).json("Password changed successfully!");
   } catch (error: any) {
     if (error.statusCode === 400) {
       res.status(error.statusCode).json({ error: error.message });
@@ -50,4 +80,5 @@ const forgetPassword = async (req: Request, res: Response) => {
 export default {
   login,
   forgetPassword,
+  resetPassword,
 };
