e5b752caefa1205d0f01f6d4b1d9ff2ee27c79b4
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-17 19:52:33 +0200

    Modify admin permissions as enum

--- migrations/sqls/20230417162148-admin-permissions-up.sql
+++ migrations/sqls/20230417162148-admin-permissions-up.sql
@@ -1,8 +1,10 @@
+CREATE TYPE PERMISSION AS ENUM ('CREATE', 'UPDATE', 'DELETE');
+
 CREATE TABLE admin_permissions (
     admin_id INT NOT NULL REFERENCES users(id),
-    permission VARCHAR(255) NOT NULL,
+    permission PERMISSION NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP,
     CONSTRAINT PK PRIMARY KEY (admin_id, permission)
 );
