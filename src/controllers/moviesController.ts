5b80c0e247b5ea8c8c724854da4ddbe5edc3ade0
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-24 21:07:29 +0200

    Finalize getMovie

--- src/controllers/moviesController.ts
+++ src/controllers/moviesController.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
+import { Watchlist } from "../models/Watchlist";
+import { getMovieDetails } from "../services/movieServices";
+
+const watchlist = new Watchlist();
 
 const getMovie = async (req: Request, res: Response) => {
   try {
-    const id = req.params.id as unknown as number;
+    const movieId = req.params.id as unknown as number;
+    const userId = req.payload?.userId;
+    const movie = await getMovieDetails(movieId, userId ?? undefined);
+    res.status(200).json(movie);
   } catch (error: any) {
     if (error.statusCode) {
       res.status(error.statusCode).json({ error: error.message });
