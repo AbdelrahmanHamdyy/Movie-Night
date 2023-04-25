d15b430fafe530eab7fb582b33cac3de95b8e776
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-25 06:36:39 +0200

    More movie validators

--- src/validators/movieValidators.ts
+++ src/validators/movieValidators.ts
@@ -18,6 +18,27 @@ export const createMovieValidator = [
     .withMessage("Movie title can't be empty")
     .isAlphanumeric()
     .withMessage("Movie title must be letters & numbers only"),
+  body("language")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Movie language can't be empty")
+    .isAlpha()
+    .withMessage("Movie language must be letters only"),
+  body("country")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Movie country can't be empty")
+    .isAlpha()
+    .withMessage("Movie country must be letters only"),
+  body("duration")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Movie duration can't be empty")
+    .isNumeric()
+    .withMessage("Movie duration must be numeric"),
   body("about")
     .trim()
     .not()
@@ -32,4 +53,21 @@ export const createMovieValidator = [
     .withMessage("ID can't be empty")
     .isNumeric()
     .withMessage("ID must be a number"),
+];
+
+export const movieGenresValidator = [
+  body("id")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Movie ID can't be empty")
+    .isNumeric()
+    .withMessage("Movie ID must be a number"),
+  body("genres")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Genres array can't be empty")
+    .isArray({ min: 1 })
+    .withMessage("A movie must have atleast 1 genre"),
 ];
