fc3c1b5cd125cb9071b3e754913dd5c99d979274
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-06-20 17:58:19 +0300

    Add optional validators

--- src/validators/movieValidators.ts
+++ src/validators/movieValidators.ts
@@ -90,18 +90,27 @@ export const updateMovieValidator = [
     .withMessage("Movie ID can't be empty")
     .isNumeric()
     .withMessage("Movie ID must be a number"),
-  body("budget").isNumeric().withMessage("Budget must be a number"),
-  body("releaseDate").isDate().withMessage("Release Date must be a date"),
+  body("budget").optional().isNumeric().withMessage("Budget must be a number"),
+  body("releaseDate")
+    .optional()
+    .isDate()
+    .withMessage("Release Date must be a date"),
   body("title")
+    .optional()
     .isAlphanumeric()
     .withMessage("Title must be letters or numbers only"),
   body("about")
+    .optional()
     .isAlphanumeric()
     .withMessage("About must be in letters or numbers only"),
-  body("duration").isNumeric().withMessage("Duration must be a number"),
-  body("language").isAlpha().withMessage("Language must be alpha"),
-  body("country").isAlpha().withMessage("Country must be alpha"),
+  body("duration")
+    .optional()
+    .isNumeric()
+    .withMessage("Duration must be a number"),
+  body("language").optional().isAlpha().withMessage("Language must be alpha"),
+  body("country").optional().isAlpha().withMessage("Country must be alpha"),
   body(["directorId", "producerId", "companyId"])
+    .optional()
     .isNumeric()
     .withMessage("ID must be a number"),
 ];
