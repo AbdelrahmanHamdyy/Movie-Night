1cbae6ce1bfa83c23d98baa74f0a4fecac00aabd
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-06-20 17:58:29 +0300

    Edit update query for missing attributes

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -83,7 +83,7 @@ export class Movie {
     try {
       const conn = await client.connect();
       const sql =
-        "UPDATE movies SET title=$1, about=$2, language=$3, trailer_url=$4, score=$5, rating=$6, budget=$7, release_date=$8, director_id=$9, producer_id=$10, company_id=$11, cover_url=$12 WHERE id=$13;";
+        "UPDATE movies SET title=$1, about=$2, language=$3, trailer_url=$4, score=$5, rating=$6, budget=$7, release_date=$8, director_id=$9, producer_id=$10, company_id=$11, cover_url=$12, country=$13, duration=$14 WHERE id=$15;";
 
       const result = await conn.query(sql, [
         movie.title,
@@ -98,6 +98,8 @@ export class Movie {
         movie.producer_id,
         movie.company_id,
         movie.cover_url,
+        movie.country,
+        movie.duration,
         movie.id,
       ]);
       conn.release();
