c9c72824e5758446c7772822876b73605a21b1ce
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-06-26 04:37:55 +0300

    Watchlist validators

---
+++ src/validators/watchlistValidators.ts
@@ -0,0 +1,26 @@
+import { body, check, param, query } from "express-validator";
+
+export const getWatchlistValidator = [
+  query("skip")
+    .not()
+    .isEmpty()
+    .withMessage("Skip parameter cannot be empty!")
+    .isNumeric()
+    .withMessage("Skip must be a number!"),
+  query("limit")
+    .not()
+    .isEmpty()
+    .withMessage("Limit parameter cannot be empty!")
+    .isNumeric()
+    .withMessage("Limit must be a number!"),
+];
+
+export const movieIdValidator = [
+  body("movieId")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Movie ID can't be empty")
+    .isNumeric()
+    .withMessage("Movie ID must be a number"),
+];
