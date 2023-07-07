d56fe4b530fd78d9bf1d20ab4be5fe1a8394f6d1
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-07-07 05:07:42 +0300

    Add more db functions to User rating model

--- src/models/UserRating.ts
+++ src/models/UserRating.ts
@@ -32,6 +32,28 @@ export class UserRating {
     }
   }
 
+  async update(userRating: UserRatingData): Promise<UserRatingData> {
+    try {
+      const conn = await client.connect();
+      const sql =
+        "UPDATE user_ratings SET rate=$1 WHERE user_id=$2 AND movie_id=$3;";
+
+      const result = await conn.query(sql, [
+        userRating.rate,
+        userRating.user_id,
+        userRating.movie_id,
+      ]);
+      conn.release();
+
+      return result.rows[0];
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        `Failed to update movie ${userRating.movie_id} rating for user ${userRating.user_id}!`
+      );
+    }
+  }
+
   async getRating(userId: number, movieId: number): Promise<number> {
     try {
       const conn = await client.connect();
@@ -53,6 +75,41 @@ export class UserRating {
     }
   }
 
+  async exists(userId: number, movieId: number): Promise<boolean> {
+    try {
+      const conn = await client.connect();
+      const sql =
+        "SELECT EXISTS (SELECT * FROM user_ratings WHERE user_id=$1 AND movie_id=$2);";
+
+      const result = await conn.query(sql, [userId, movieId]);
+      conn.release();
+
+      return result.rows[0];
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        `Failed to check if user ${userId} rating exists for movie ${movieId}!`
+      );
+    }
+  }
+
+  async getNumberOfRatings(movieId: number): Promise<number> {
+    try {
+      const conn = await client.connect();
+      const sql = "SELECT COUNT(*) FROM user_ratings WHERE movie_id=$1;";
+
+      const result = await conn.query(sql, [movieId]);
+      conn.release();
+
+      return result.rows[0];
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        `Failed to retrieve the number of people who rated movie ${movieId}!`
+      );
+    }
+  }
+
   async index(userId: number): Promise<UserRating[]> {
     try {
       const conn = await client.connect();
@@ -66,6 +123,22 @@ export class UserRating {
     } catch (error) {
       console.error(error);
       throw new Error(`Failed to index all movies rated by user ${userId}!`);
+    }
+  }
+
+  async get(movieId: number): Promise<UserRating[]> {
+    try {
+      const conn = await client.connect();
+      const sql =
+        "SELECT * FROM user_ratings WHERE movie_id=$1 ORDER BY created_at DESC;";
+
+      const movies = await conn.query(sql, [movieId]);
+      conn.release();
+
+      return movies.rows;
+    } catch (error) {
+      console.error(error);
+      throw new Error(`Failed to index all ratings of movie ${movieId}!`);
     }
   }
 }
