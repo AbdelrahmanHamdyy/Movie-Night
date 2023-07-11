d861d56604af2e4f68ab0a3399ad3aaf4bc9f70f
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-07-11 10:59:02 +0300

    Add description for reviews

--- migrations/sqls/20230417172816-reviews-up.sql
+++ migrations/sqls/20230417172816-reviews-up.sql
@@ -2,7 +2,7 @@ CREATE TABLE reviews (
     user_id INT NOT NULL REFERENCES users(id),
     movie_id INT NOT NULL REFERENCES movies(id),
     fav_actor_id INT NOT NULL REFERENCES film_makers(id),
-    review TEXT NOT NULL,
+    description TEXT NOT NULL,
     spoiler BOOLEAN DEFAULT FALSE,
     recommended BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
