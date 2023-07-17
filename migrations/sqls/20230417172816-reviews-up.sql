a434a7ae229a724a21df05c9981612f1a08b5b9c
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-07-17 04:51:42 +0300

    Add likes and dislikes to reviews table

--- migrations/sqls/20230417172816-reviews-up.sql
+++ migrations/sqls/20230417172816-reviews-up.sql
@@ -3,6 +3,8 @@ CREATE TABLE reviews (
     movie_id INT NOT NULL REFERENCES movies(id),
     fav_actor_id INT NOT NULL REFERENCES film_makers(id),
     description TEXT NOT NULL,
+    likes INT DEFAULT 0,
+    dislikes INT DEFAULT 0,
     spoiler BOOLEAN DEFAULT FALSE,
     recommended BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
