a15a97c1595e20b372b2473cb7cc3deb2cf283ae
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-07-24 00:35:31 +0300

    Get movie actors & writers initial controllers

---
+++ src/controllers/movieWriterController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+const getMovieWriters = async (req: Request, res: Response) => {
+  try {
+    // TODO
+  } catch (error: any) {
+    if (error.statusCode) {
+      res.status(error.statusCode).json({ error: error.message });
+    } else {
+      res.status(500).json("Internal server error");
+    }
+  }
+};
+
+export default {
+  getMovieWriters,
+};
